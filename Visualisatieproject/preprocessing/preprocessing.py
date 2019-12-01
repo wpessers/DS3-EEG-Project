710aecdb231a728c0e3d4cdb8b976259bd8143ab
Warre Pessers <warre.pesser@student.kdg.be>
2019-12-02 00:07:51 +0100

    Added commonly used statistics for data analysis:     - Minimum     - Maximum     - Mean     - Median     - Standard deviation     - Variance

--- Visualisatieproject/preprocessing/preprocessing.py
+++ Visualisatieproject/preprocessing/preprocessing.py
@@ -15,11 +15,9 @@ class Preprocessing():
         csv_dir = self._data_dir + "input/"
         self.process_csv_files(csv_dir)
 
-
     def organize_dirs(self):
         if not os.path.exists('../res/output'):
             os.makedirs('../res/output')
-
 
     def process_csv_files(self, csv_dir):
         csv_path = Path(csv_dir).glob("**/*.csv")
@@ -30,7 +28,6 @@ class Preprocessing():
             df = df[df["INTERPOLATED"].notnull()]
             self.dataframe_to_json(df, person_name)
 
-
     def dataframe_to_json(self, df, person_name, rows=256):
         for row in df.itertuples(index=True, name='Pandas'):
             if "stimulus" in getattr(row, "COUNTER") and "*" not in getattr(row, "INTERPOLATED"):
@@ -40,25 +37,35 @@ class Preprocessing():
                 json_data["name"] = person_name
                 json_data["stimulus"] = stimulus
 
-                for i in range((len(self._electrode_list)//2)):
-                    json_data[self._electrode_list[i * 2]] = self.placeholder_function(df, index + 1, index + rows,
-                                                                                       self._electrode_list[(i * 2) + 1])
+                for i in range((len(self._electrode_list) // 2)):
+                    json_data[self._electrode_list[i * 2]] = self.get_electrodes_data(df, index + 1, index + rows,
+                                                                                      self._electrode_list[(i * 2) + 1])
 
                 self.write_json_file(json_data, person_name, stimulus)
-
 
     def get_metadata(self, row):
         stimulus = getattr(row, "INTERPOLATED")
         index = getattr(row, "Index")
         return stimulus, index
 
-
-    def placeholder_function(self, df, start, end, electrodes):
-        dict = {}
+    def get_electrodes_data(self, df, start, end, electrodes):
+        electrode_group_dict = {}
         for electrode in electrodes:
-            dict[electrode] = json.loads(df.loc[start:end, electrode].to_json(orient="records"))
-        return dict
+            electrode_dict = {}
+            raw_df = df.loc[start:end, electrode]
+            electrode_dict["data"] = json.loads(raw_df.to_json(orient="records"))
+            electrode_dict["stats"] = self.get_statistics(raw_df)
+            electrode_group_dict[electrode] = electrode_dict
+        return electrode_group_dict
 
+    def get_statistics(self, df: pd.DataFrame):
+        stats_dict = {"min": df.min(),
+                      "max": df.max(),
+                      "mean": df.mean(),
+                      "median": df.median(),
+                      "stddev": df.std(ddof=0),
+                      "variance": df.var(ddof=0)}
+        return stats_dict
 
     def write_json_file(self, dict, name, stimulus):
         self.organize_dirs()
